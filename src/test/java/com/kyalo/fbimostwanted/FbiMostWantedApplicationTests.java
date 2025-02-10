d52ddb7deeba776752cf028611fe063ab56ef351
kyalo-vibes <kevin.kioko4@gmail.com>
2025-02-10 18:50:31 +0300

    Project set up

---
+++ src/test/java/com/kyalo/fbimostwanted/FbiMostWantedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyalo.fbimostwanted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FbiMostWantedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
